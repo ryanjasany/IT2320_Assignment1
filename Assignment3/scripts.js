98716e8eae15ce7ab9d5e3f1ae473334f1ef34ca
Ryan Jasany <ryanjasany6@gmail.com>
2017-09-13 11:13:03 -0400

    added Invert Page button.  Added on click function for table.

--- Assignment3/scripts.js
+++ Assignment3/scripts.js
@@ -16,3 +16,54 @@ function PersonalizePage(input){
 	page.innerHTML = input.value;
 
 }
+
+function AlterText(div){
+
+	if(div.className == "badDescription")
+	{
+		div.className = "red";
+	}
+	else
+	{
+		div.className = "badDescription";
+	}
+}
+
+function InvertColors(div){
+
+	if(div.className == "invertButton")
+	{
+		var page = document.getElementsByClassName("coffeeQuickGuide")[0];
+
+		page.className = "invertedPage"
+
+		var header = document.getElementsByClassName("header")[0];
+
+		header.className = "invertedHeader"
+
+		var revert = document.getElementsByClassName("invertButton")[0];
+
+		revert.className = "revertButton";
+	}
+	else
+	{
+		var page = document.getElementsByClassName("invertedPage")[0];
+
+		page.className = "coffeeQuickGuide"
+
+		var header = document.getElementsByClassName("invertedHeader")[0];
+
+		header.className = "header"
+
+		var revert = document.getElementsByClassName("revertButton")[0];
+
+		revert.className = "invertButton";
+	
+	
+	
+	
+	}
+
+
+
+}
