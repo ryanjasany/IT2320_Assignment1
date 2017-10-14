ee4ef85a27201ec0758a639e3d33a20a45c1ec4d
Ryan Jasany <ryanjasany6@gmail.com>
2017-10-13 20:38:08 -0400

    added behavior to display json details

--- Assignment7/scripts.js
+++ Assignment7/scripts.js
@@ -6,44 +6,85 @@ var Main = {}
 		"Year":2017,
 		"months":[
 			{
-				"January":{
-					holidays:["New Year's Day","National Bird Day","Opposite Day","Chinese New Years"]
-				}
+				"name":"January",
+				"holidays":["New Year's Day","National Bird Day","Opposite Day","Chinese New Years"],
+				"numberOfDays":31
+				
+				
 			},
 			{
-				"February":{
-				holidays:["Groundhog's Day","Super Bowl Sunday","Valentine's Day","Mardi Gras"]
-				}
+				"name":"February",
+				"holidays":["Groundhog's Day","Super Bowl Sunday","Valentine's Day","Mardi Gras"],
+				"numberOfDays":28
 			},
 			{
-				"March":{
-				holidays:["","","",""]
-				}
+				"name":"March",
+				"holidays":["International Women's Day","Ides of March","St. Patrick's Day","Spring Equinox"],
+				"numberOfDays":31
 			},
 			{
-				"April":{
-				holidays:["","","",""]
-				}
+				"name":"April",
+				"holidays":["April Fool's Day","Earth Day","Arbor Day","Easter Sunday"],
+				"numberOfDays":30
+				
 			},
 			{
-				"May":{
-				holidays:["","","",""]
-				}
+				"name":"May",
+				holidays:["May Day","Star Wars Day","Cinco de Mayo","Mother's Day"],
+				"numberOfDays":31
 				}
 		]		
 			
-  
     }
     
 
+	Main.produceButtons = function(){
+	
+		for(var x = 0; x < Main.calendar.months.length; x++)
+		{
+			$(".display").append("<li class='month'>" + Main.calendar.months[x].name + "</li>")
+		}
+	
+	}
+
+	Main.produceSummary = function(name){
+	
+		var htmlAndText = "";
+
+		for(var x = 0; x < Main.calendar.months.length; x++)
+		{
+			if(Main.calendar.months[x].name == name)
+			{
+				htmlAndText += "Name: " + Main.calendar.months[x].name + "<br>";
+				htmlAndText += "Number of Days: " + Main.calendar.months[x].numberOfDays + "<br>";
+				htmlAndText += "Holidays: " + Main.calendar.months[x].holidays.length + "<br>";
+
+				for(var y = 0; y < Main.calendar.months[x].holidays.length; y++)
+				{
+					htmlAndText += "<li>" + Main.calendar.months[x].holidays[y] + "</li>"
+				
+				}
+
+				return htmlAndText;
+
+			}
+		}
+
+	}
 
 
 $(document).ready(function () {
 
 
-	$(".display").text(Main.calendar.Year);
+	$(".header").append(Main.calendar.Year);
     
+	Main.produceButtons();
 
+	$(".month").click(function(){
+
+		$(".details").html(Main.produceSummary($(this).text()));
+
+	});
 
 
 });
