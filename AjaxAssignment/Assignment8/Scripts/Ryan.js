77a3cc8aaca34ff49369a724d85122b634b1c4ea
Ryan Jasany <ryanjasany6@gmail.com>
2017-10-23 18:40:41 -0400

    assignment 8 - ajax Ryan.js

---
+++ AjaxAssignment/Assignment8/Scripts/Ryan.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+	var entry;
+	var textbox;
+
+	$(".player-number-button").click(function () {
+
+		alert("Contacting Server");
+		entry = $(".player-number-textbox-input").val();
+		textbox = $(".output");
+
+		$.ajax({
+			url: "Home/GetPlayerInformation",
+			data: { PlayerNumber: entry },
+			success: function (stringResponse) {
+				response = JSON.parse(stringResponse);
+				textbox.append("Name: " + response.PlayerName + "<br>Number: " + response.PlayerNumber + "<br>");
+				textbox.css("border", "1px solid green");
+			}
+
+		});
+
+	});
+
+
+});
