e4364c8bf3e7faf5118794fef08fcb58295e238c
Ryan Jasany <ryanjasany6@gmail.com>
2017-10-31 21:27:39 -0400

    update html, create account call, and basic styling

--- Week 10 - Final Project/FinalProject/Scripts/Home.js
+++ Week 10 - Final Project/FinalProject/Scripts/Home.js
@@ -1,1 +1,43 @@
-﻿
+﻿var Home = {};
+
+Home.createAccount = function() {
+
+    var username;
+    var password;
+    var email;
+    var repeatEmail;
+
+    alert("Contacting Server");
+
+    var username = $(".create.username").val();
+    var password = $(".create.password").val();
+    var email = $(".create.email").val();
+    var repeatEmail = $(".create.repeatEmail").val();
+
+
+    $.ajax({
+        url: "Home/CreateAccount",
+        data: {
+            "Username": username,
+            "Password": password,
+            "EmailAdd": email,
+            "EmailCon": repeatEmail
+        },
+        success: function (stringResponse) {
+            response = JSON.parse(stringResponse);
+            alert(response.Message);
+        }
+
+    });
+
+}
+
+
+$(document).ready(function () {
+
+    $(".createButton").click(function()
+    {
+        Home.createAccount()
+    });
+
+});
