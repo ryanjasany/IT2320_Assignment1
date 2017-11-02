1d738fc3065ce59bed24552ef54706929a6b68cd
Ryan Jasany <ryanjasany6@gmail.com>
2017-11-01 21:36:35 -0400

    update styling, add login call

--- Week 10 - Final Project/FinalProject/Scripts/Home.js
+++ Week 10 - Final Project/FinalProject/Scripts/Home.js
@@ -7,8 +7,6 @@ Home.createAccount = function() {
     var email;
     var repeatEmail;
 
-    alert("Contacting Server");
-
     var username = $(".create.username").val();
     var password = $(".create.password").val();
     var email = $(".create.email").val();
@@ -25,7 +23,42 @@ Home.createAccount = function() {
         },
         success: function (stringResponse) {
             response = JSON.parse(stringResponse);
-            alert(response.Message);
+            if (response.Message === "Error")
+            {
+                $(".error.create").css("display", "block");
+            } else if (response.Message === "Success") {
+                $(".container").css("border", "1px solid green");
+            }
+        }
+
+    });
+
+}
+
+
+Home.logIn = function () {
+
+    var username;
+    var password;
+
+    var username = $(".logIn.username").val();
+    var password = $(".logIn.password").val();
+
+
+    $.ajax({
+        url: "Home/Login",
+        data: {
+            "Username": username,
+            "Password": password
+        },
+        success: function (stringResponse) {
+            response = JSON.parse(stringResponse);
+            if (response.Message === "Error") {
+                $(".error.logIn").css("display", "block");
+            }
+            else if (response.Message === "Success") {
+                $(".container").css("border", "1px solid green");
+            }
         }
 
     });
@@ -38,6 +71,25 @@ $(document).ready(function () {
     $(".createButton").click(function()
     {
         Home.createAccount()
+    });
+
+    $(".logInButton").click(function () {
+        Home.logIn()
+    });
+
+
+    $(".createButton, .logInButton").mouseenter(function () {
+
+        $(this).css("background-color", "lightyellow");
+
+
+    });
+
+    $(".createButton, .logInButton").mouseleave(function () {
+
+
+        $(this).css("background-color", "lightblue");
+
     });
 
 });
