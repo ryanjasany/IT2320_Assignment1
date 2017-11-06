1f4d1196ba73c4795319758c34e349264cb83572
Ryan Jasany <ryanjasany6@gmail.com>
2017-11-05 19:57:40 -0500

    add update email and gpa functionality

--- Week 10 - Final Project/FinalProject/Scripts/Home.js
+++ Week 10 - Final Project/FinalProject/Scripts/Home.js
@@ -94,8 +94,73 @@ Home.addOrUpdateElement = function () {
             }
             else if (response.Message === "Success") {
                 $(".container").css("border", "1px solid green");
-                alert("success");
                 $(".additionalInfo").append("<li>Name: " + elementName + "        Value: " + elementValue + "</li>");
+
+            }
+        }
+
+    });
+
+}
+
+Home.updateEmail = function () {
+
+    var username;
+    var elementName;
+    var elementValue;
+
+    username = $(".displayName").text();
+    elementName = "emailadd";
+    elementValue = $("input.account.email").val();
+
+
+    $.ajax({
+        url: "Home/AddOrUpdateElement",
+        data: {
+            "Username": username,
+            "elementName": elementName,
+            "elementValue": elementValue
+        },
+        success: function (stringResponse) {
+            response = JSON.parse(stringResponse);
+            if (response.Message === "Error") {
+                alert("error");
+            }
+            else if (response.Message === "Success") {
+                $(".container").css("border", "1px solid green");
+
+            }
+        }
+
+    });
+
+}
+
+Home.updateGPA = function () {
+
+    var username;
+    var elementName;
+    var elementValue;
+
+    username = $(".displayName").text();
+    elementName = "gpa";
+    elementValue = $("input.account.gpa").val();
+
+
+    $.ajax({
+        url: "Home/AddOrUpdateElement",
+        data: {
+            "Username": username,
+            "elementName": elementName,
+            "elementValue": elementValue
+        },
+        success: function (stringResponse) {
+            response = JSON.parse(stringResponse);
+            if (response.Message === "Error") {
+                alert("error");
+            }
+            else if (response.Message === "Success") {
+                $(".container").css("border", "1px solid green");
 
             }
         }
@@ -107,7 +172,6 @@ Home.addOrUpdateElement = function () {
 Home.getInformation = function () {
 
     var username;
-
     var username = $(".logIn.username").val();
 
 
@@ -128,10 +192,11 @@ Home.getInformation = function () {
 
                 $(".displayName").text(response2.account.username);
                 $(".account.email").val(response2.account.emailadd);
+                $(".account.gpa").val(response2.account.gpa);
 
                 for (x in response2.account)
                 {
-                    if (!(x === "username" || x === "password" || x === "emailadd")) {
+                    if (!(x === "username" || x === "password" || x === "emailadd" || x === "gpa")) {
                         $(".additionalInfo").append("<li>Name: " + x + "        Value: "+  response2.account[x] + "</li>");
                     }
                     }
@@ -157,8 +222,14 @@ $(document).ready(function () {
         Home.logIn()
     });
 
+    $(".button.update.email").click(function () { Home.updateEmail() });
+
     $("button.addElement").click(function () {
         Home.addOrUpdateElement()
+    });
+
+    $("button.update.gpa").click(function () {
+        Home.updateGPA()
     });
 
     $(".createButton, .logInButton, .update, .addElement").mouseenter(function () {
