5a0ac6c543852cb7edf0cc6a4b0c5e18e7d2b637
Ryan Jasany <ryanjasany6@gmail.com>
2017-11-08 14:48:02 -0500

    improve functionality

--- Assignment11_Canvas/Scripts.js
+++ Assignment11_Canvas/Scripts.js
@@ -9,61 +9,76 @@ Main.focusWidth = 90;
 Main.focusHeight = 90;
 Main.speed = 2;
 Main.pause = false;
+Main.followMe = false;
+Main.initX = 0;
+Main.initY = 300;
+Main.offset = 120;
+Main.focusNumber = 0;
+Main.temp = 0;
 
-Main.Box = function (xC, yC, c, d) {
+Main.Box = function (xC, yC, c, d, index) {
 
     this.x = xC;
     this.y = yC;
     this.switch = false;
+    this.number = index;
 
     this.draw = function () {
 
-        if (Main.MX < this.x + 40 && Main.MX > this.x - 40 && Main.MY > 300 && Main.MY < 400) {
+        //If mouseover
+        if (Main.MX < this.x + 30 && Main.MX > this.x - 30 && Main.MY > 300 && Main.MY < 400) {
             Main.Context.fillStyle = "#FF0000";
             Main.Context.fillRect(this.x, this.y, Main.focusHeight, Main.focusWidth);
-            Main.speed = 0;
-            //Main.pause = true;
+            this.switch = true;
+
+            //If mousedown
+            if (Main.followMe == true) {
+
+                Main.focusNumber = this.number;
+                this.x = Main.MX;
+                Main.Context.fillRect(Main.MX, this.y, Main.focusHeight, Main.focusWidth);
+                Main.temp = this.x;
+
+            }
         }
         else {
+            //If not under mouse
             Main.Context.fillStyle = "#FFFF00";
-            Main.Context.fillRect(this.x, this.y, c, d);
-            Main.speed = 2;
-            //this.pause = false;
-        }
-        
+            this.switch = false;
+            if (Main.followMe == true) {
 
-        if (Main.pause == false) {
+                var newLocation = Main.temp + Main.offset;
+                Main.temp = newLocation;
+                this.x = newLocation;
+
+                Main.Context.fillRect(this.x, this.y, c, d);
+            }
+            else {
+                Main.Context.fillRect(this.x, this.y, c, d);
+            }
+
+        }    
             this.x += Main.speed;
-        }
 
-        if (this.x == 600) {
-            this.x = 0;
-        }
+            if (this.x >= 600) {
+
+                this.x = this. x - 600;
+            }
 
     }
 }
 
-Main.MouseUp = function (mouseEvent) {
 
-    Main.pause = true;
+Main.Boxes = [new Main.Box(Main.initX, Main.initY, 75, 80, 0), new Main.Box(Main.initX + (Main.offset), 300, 75, 80, 1), new Main.Box(Main.initX + (Main.offset * 2), 300, 75, 80, 2), new Main.Box(Main.initX + (Main.offset * 3), 300, 75, 80, 3), new Main.Box(Main.initX + (Main.offset) * 4, 300, 75, 80, 4)]
+
+
+Main.MouseUp = function (mouseEvent) {
+    Main.MousePressed = false;
 }
 
 Main.MouseDown = function (mouseEvent) {
-    Main.pause = false;
+    Main.MousePressed = true;
 }
-
-Main.MouseOver = function () {
-
-
-}
-
-Main.box1 = new Main.Box(0, 300, 75, 80);
-Main.box2 = new Main.Box(120, 300, 75, 80);
-Main.box3 = new Main.Box(240, 300, 75, 80);
-Main.box4 = new Main.Box(360, 300, 75, 80);
-Main.box5 = new Main.Box(480, 300, 75, 80);
-
-
 
 Main.Canvas.onmousemove = function(event)
 {
@@ -83,6 +98,38 @@ Main.Canvas.onmousemove = function(event)
 }
 
 
+Main.checkForPause = function () {
+
+    var total = 0;
+
+    for (var i = 0; i < Main.Boxes.length; i++) {
+
+        if (Main.Boxes[i].switch) {
+            Main.pause = true;
+            Main.speed = 0;
+        }
+        else {
+            total++;
+        }
+    }
+
+    if (total == 5) {
+        Main.pause = false;
+        Main.speed = 2;
+    }
+}
+
+Main.holdBlock = function () {
+
+    if (Main.pause == true && Main.MousePressed == true) {
+        Main.followMe = true;
+    }
+    else {
+        Main.followMe = false;
+    }
+
+}
+
 
 
 
@@ -90,18 +137,20 @@ Main.Animate = function()
 {
 	Main.Context.clearRect(0, 0, Main.Canvas.width, Main.Canvas.height);
 
+    Main.checkForPause();
+    Main.holdBlock();
 
-    Main.box1.draw();
-    Main.box2.draw();
-    Main.box3.draw();
-    Main.box4.draw();
-    Main.box5.draw();
+
+    for (var i = 0; i < Main.Boxes.length; i++)
+    {
+        Main.Boxes[i].draw();
+    }
 
 
 
     //Output coordinate
 	Main.Context.font = "30px Arial";
-	Main.Context.fillText("X: " + Main.MX + "  Y: " + Main.MY, 100, 150);
+	Main.Context.fillText("X: " + Main.MX + "  Y: " + Main.MY + " Speed is " + Main.speed + " " + Main.MousePressed, 100, 150);
 
 	requestAnimFrame(function() { Main.Animate(); });
 }
@@ -121,9 +170,6 @@ window.requestAnimFrame = (function(callback)
 $(document).ready(function()
 {
     Main.Animate();
-    //Main.Canvas.addEventListener('mouseup', function (mouseEvent) { Main.MouseUp(mouseEvent); });
-    //Main.Canvas.addEventListener('mousedown', function (mouseEvent) { Main.MouseDown(mouseEvent); });
-    Main.Canvas.addEventListener('mouseenter', function (mouseEvent) { Main.MouseUp(mouseEvent); });
-    Main.Canvas.addEventListener('mouseleave', function (mouseEvent) {Main.MouseDown(mouseEvent);});
-
+    Main.Canvas.addEventListener('mouseup', function (mouseEvent) { Main.MouseUp(mouseEvent); });
+    Main.Canvas.addEventListener('mousedown', function (mouseEvent) { Main.MouseDown(mouseEvent); });
 });
